9f996cc46a410c859657cc846df42148567726fa
Gaellan <doucet.mari@gmail.com>
2023-02-28 15:19:15 +0100

    restrincting user update

--- users/update.js
+++ users/update.js
@@ -1,4 +1,5 @@
 const { Sequelize, Model, DataTypes } = require("sequelize");
+const bcrypt  = require("bcrypt");
 const sequelize = new Sequelize({
     dialect : "sqlite",
     storage : "./database.sqlite3"
@@ -38,16 +39,79 @@ module.exports = function(req,res) {
 
             req.on("end", async () => {
                 let fields = JSON.parse(body);
-                console.log(fields);
-                User.update(fields, {
-                    where : {
-                        id : id
-                    }
-                }).then((data) => {
-                    console.log(data);
-                    res.writeHead(200, { "Content-Type": "application/json" });
-                    res.end(JSON.stringify(data));
-                });
+
+                if(fields.email === fields.callerEmail)
+                {
+                    console.log("I am updating myself\n");
+                    User.findOne(
+                        {
+                            raw: true,
+                            where : {
+                                email : fields.email
+                            }
+                        }
+                    ).then((caller) => {
+
+                        let plainPassword = fields.callerPassword;
+
+                        bcrypt.compare(plainPassword, caller.password)
+                            .then(result => {
+                                User.update({ firstName : fields.firstName, lastName : fields.lastName}, {
+                                    where : {
+                                        id : id
+                                    }
+                                }).then((data) => {
+                                    console.log(data);
+                                    res.writeHead(200, { "Content-Type": "application/json" });
+                                    res.end(JSON.stringify(data));
+                                });
+                            })
+                            .catch(err => {
+                                console.log(err)
+                            });
+                    });
+                }
+                else
+                {
+                    console.log("I am updating someone else");
+                    // retrieve the caller user in database from its email
+                    User.findOne(
+                        {
+                            raw: true,
+                            where : {
+                                email : fields.callerEmail
+                            }
+                        }
+                    ).then((caller) => {
+
+                        let plainPassword = fields.callerPassword;
+
+                        bcrypt.compare(plainPassword, caller.password)
+                            .then(result => {
+
+                                if(caller.role === "ADMIN")
+                                {
+                                    User.update({ firstName : fields.firstName, lastName : fields.lastName}, {
+                                        where : {
+                                            id : id
+                                        }
+                                    }).then((data) => {
+                                        console.log(data);
+                                        res.writeHead(200, { "Content-Type": "application/json" });
+                                        res.end(JSON.stringify(data));
+                                    });
+                                }
+                                else
+                                {
+                                    res.writeHead(403, { "Content-Type": "application/json" });
+                                    res.end(JSON.stringify("You do not have enough privileges to update another user"));
+                                }
+                            })
+                            .catch(err => {
+                                console.log(err)
+                            });
+                    });
+                }
             });
         } catch (error) {
             res.writeHead(500, { "Content-Type": "application/json" });
